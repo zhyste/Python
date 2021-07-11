9b8bcdff14cde1676f125197684aa5451fa0410b
zhyste <53603889+zhyste@users.noreply.github.com>
2021-07-11 15:25:53 +0800

    Update Spam code - Copy.py

--- Projects/Spam code - Copy.py
+++ Projects/Spam code - Copy.py
@@ -7,7 +7,7 @@ time.sleep(2)
 
 while x<100:
     time.sleep(0.1)
-    keyboard.type ("i like eMath ")
+    keyboard.type ("Whatever you want your keyboard to press ")
     keyboard.press(Key.enter)
     keyboard.release(Key.enter)
     x=x+1
